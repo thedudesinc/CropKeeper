3b2b0f61424b2400e15051fdbfd1a4573683f554
Zachary Pruessner <zachpruessner@gmail.com>
2023-03-27 22:21:56 -0500

    login and signup page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CropKeeper';
+
+  constructor(private renderer: Renderer2) {
+  }
+
+  ngOnInit(): void {
+    initFlowbite();
+    this.initTheme();
+  }
+
+  initTheme(): void {
+    const theme = localStorage.getItem('theme');
+    this.renderer.addClass(document.body, theme || 'dark');
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  changeTheme(event: KeyboardEvent): void {
+    if (event.keyCode !== 37) return;
+
+    const theme = localStorage.getItem('theme') || 'dark';
+    const newTheme = theme === 'dark' ? 'light' : 'dark';
+
+    ['light', 'dark'].forEach((t) => this.renderer.removeClass(document.body, t));
+
+    this.renderer.addClass(document.body, newTheme);
+    localStorage.setItem('theme', newTheme);
+  }
 }
