47ec44e8ec9b5e1a9bd06458029271bd0e9007a6
Zachary Pruessner <zachpruessner@gmail.com>
2023-04-08 21:38:36 -0500

    email in use implement

--- src/app/layout/signup/signup.component.ts
+++ src/app/layout/signup/signup.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, AsyncValidatorFn, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { ControlsOf } from 'src/app/helpers/helper.types';
 import { LoadingService } from 'src/app/services/loading.service';
 import { UserInput } from 'src/app/services/models/user.model';
 import { UserService } from 'src/app/services/user.service';
 import { Router } from '@angular/router';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
@@ -16,7 +17,7 @@ export class SignupComponent {
 
   userForm: FormGroup<ControlsOf<UserInput>> = new FormGroup<ControlsOf<UserInput>>({
     displayName: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.minLength(2), Validators.maxLength(50)] }),
-    email: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.email] }),
+    email: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.email], asyncValidators: [EmailValidator.createValidator(this.userService)] }),
     password: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.minLength(10), Validators.maxLength(100)] }),
     zip: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.pattern(/^\d{5}(?:[-\s]\d{4})?$/)] }),
     allowEmailNotifications: new FormControl(false, { nonNullable: true, validators: [Validators.required] }),
@@ -28,13 +29,27 @@ export class SignupComponent {
   get password() { return this.userForm.get('password'); }
   get zip() { return this.userForm.get('zip'); }
 
-  constructor(private userservice: UserService, private loadingService: LoadingService, private router: Router) { }
+  constructor(private userService: UserService, private loadingService: LoadingService, private router: Router) { }
 
   onSubmit(): void {
     this.loadingService.isLoadingVisible.next(true);
-    this.userservice.create(this.userForm.getRawValue()).subscribe((response) => {
+    this.userService.create(this.userForm.getRawValue()).subscribe((response) => {
       this.loadingService.isLoadingVisible.next(false);
       this.router.navigate(['/']);
     });
+  }
+}
+
+export class EmailValidator {
+  static createValidator(userService: UserService): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors | null> => {
+      return userService
+        .verifyEmail(control.value)
+        .pipe(
+          map((result: boolean) =>
+            result ? null : { emailAlreadyExists: true }
+          )
+        );
+    };
   }
 }
